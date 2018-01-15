9ec17bbcb3a7cdfcb8af1aa509e61e87f956c854
Wei Lu <luwei.here@gmail.com>
2018-01-15 13:45:03 +0800

    ex1

--- 1/ex.py
+++ 1/ex.py
@@ -1,5 +1,34 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+
 def ex1():
-    pass
+    file_path = os.path.dirname(__file__)
+    os.chdir(file_path)
+    with open("TrainExer11.txt") as f:
+        f.readline()  # skip the header
+        data = np.loadtxt(f)
+
+        # plot hist
+        ages = data[:, 1]
+        expenditures = data[:, 2]
+        fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+        axs[0].hist(ages)
+        axs[1].hist(expenditures)
+        axs[0].set_xlabel("Age")
+        axs[1].set_xlabel("Spend")
+        axs[0].set_ylabel("Frequency")
+        axs[0].set_title("Age & Spend Histogram")
+
+        # scatter plot
+        f, ax = plt.subplots()
+        ax.scatter(ages, expenditures)
+        ax.set_title('Age vs Spend')
+
+        plt.show()
 
 
 if __name__ == '__main__':
